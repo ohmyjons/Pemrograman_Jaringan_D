905ecca4c1e3a1a0a7ad77b7d4fa91e33484ff65
Ohmyjons <dohanpw@gmail.com>
2021-07-12 00:58:01 +0700

    dawdq

---
+++ progjar4a/tugas_multithreaded/multiproses.py
@@ -0,0 +1,26 @@
+from file_client_cli import remote_get
+import time
+import datetime
+from multiprocessing import Process
+
+def mengunduh():
+    texec = dict()
+    # urls = get_url_list()
+    catat_awal = datetime.datetime.now()
+    fileName = 'pokijan.jpg'
+    for k in range(100):
+        print(f"mendownload GET {[k]}")
+        waktu = time.time()
+        #mengistruksikan eksekusi fungsi download gambar secara multiprocess
+        texec[k] = Process(target=remote_get, args=(fileName,))
+        texec[k].start()
+    #setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
+    for k in range(100):
+        texec[k].join()
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+
+#fungsi download_gambar akan dijalankan secara multi process
+if __name__=='__main__':
+    mengunduh()
